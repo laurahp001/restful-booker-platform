710e829a756b97f95860ea0a807cb0aae43c3964
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2024-07-11 13:14:57 +0100

    Updated spring docs to reinstate swagger documentation

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    projectId: "9bfqi7",
+    baseUrl: "http://localhost:8080"
+  },
+});
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,5 @@
+describe('Go to Restful Booker Platform webpage', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:8080')
+  })
+})
